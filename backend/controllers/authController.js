2f16811514d37f27bd2c479c764abd723ac2891b
Safaa <Safaeettalhi@gmail.com>
2025-12-08 16:49:17 +0100

    Ajout du contrôleur Auth pour l'enregistrement et la connexion des utilisateurs avec JWT

---
+++ backend/controllers/authController.js
@@ -0,0 +1,69 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE || '7d'
+  });
+};
+
+
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({
+        message: 'Cet email est déjà utilisé'
+      });
+    }
+
+    const user = await User.create({
+      name,email,password,role: role || 'chauffeur'
+    });
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      token,
+      user: {id: user._id,name: user.name,email: user.email,role: user.role}
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: 'Veuillez fournir un email et un mot de passe'
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: 'Identifiants invalides'
+      });
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: 'Identifiants invalides'
+      });
+    }
+
+    const token = generateToken(user._id);
+     res.status(200).json({
+      token,
+      user: {id: user._id,name: user.name,email: user.email,role: user.role}
+    });
+  } catch (error) {
+    next(error);
+  }
+};
