6b24dffc39b131a993131be42daf4f441f248e78
Safaa <Safaeettalhi@gmail.com>
2025-12-09 13:48:19 +0100

    Ajout de la route /all pour récupérer tous les utilisateurs avec protection

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login ,allUsers } = require('../controllers/authController');
+const protect  = require('../middlewares/auth');
+const authorize = require('../middlewares/authorize');
 
 router.post('/register', register);
 router.post('/login', login);
-
+router.use(protect);
+router.use(authorize('admin'));
+router.get('/all', allUsers);
 module.exports = router;
