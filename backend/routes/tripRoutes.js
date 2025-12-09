30a0eb67e9bed4760d42b217872c3f8d2dd3a3dd
Safaa <Safaeettalhi@gmail.com>
2025-12-09 21:21:18 +0100

    Ajout de la route pour télécharger le PDF d’un trajet

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {getTrips,getTrip,createTrip,updateTrip,deleteTrip}= require('../controllers/tripController')
 const protect = require('../middlewares/auth');
 const authorize = require('../middlewares/authorize');
+const { downloadTripPDF } = require('../controllers/pdfController');
 
 router.use(protect);
 
 router.get('/',getTrips);
 router.get('/:id',getTrip);
 
+router.get('/:id/pdf', downloadTripPDF);
 router.use(authorize('admin'));
 
 router.post('/',createTrip);
