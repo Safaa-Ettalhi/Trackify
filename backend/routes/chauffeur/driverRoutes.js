80b21d1c5a5b5ce079f3e29738158c83b463d525
Safaa <Safaeettalhi@gmail.com>
2025-12-09 14:30:27 +0100

    Ajout des routes pour les chauffeurs pour consulter et mettre à jour leurs trajets

---
+++ backend/routes/chauffeur/driverRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getMyTrips,
+  updateTripStatus,
+  updateTripData
+} = require('../../controllers/chauffeur/driverController');
+const protect = require('../../middlewares/auth');
+const authorize = require('../../middlewares/authorize');
+
+router.use(protect);
+router.use(authorize('chauffeur'));
+
+
+router.get('/trips', getMyTrips);
+router.put('/trips/:id/status', updateTripStatus);
+router.put('/trips/:id/update', updateTripData);
+
+module.exports = router;
