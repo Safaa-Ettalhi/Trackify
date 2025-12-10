b0b1e216424d029b967bdff3fa65c5a0cb90c66c
Safaa <Safaeettalhi@gmail.com>
2025-12-10 11:04:10 +0100

    définir les variables d'environnement pour le mode test

---
+++ backend/tests/setup.js
@@ -0,0 +1,3 @@
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = '9757f6367782fe2bb94d0d064da4544e7871c64b7610af9ca495c556615438caff26e188f81312b7925d898d0da6210fc4184ef39dd99f11c61ab6d8c875e7df';
+process.env.MONGODB_URI = 'mongodb://localhost:27017/trackify_test';
