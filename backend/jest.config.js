58ed5a6b1081509be488d52d1deca09bc8c4fa0e
Safaa <Safaeettalhi@gmail.com>
2025-12-10 11:00:28 +0100

    configurer jest.config.js avec testMatch et setupFiles

---
+++ backend/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testEnvironment: 'node',
+    coveragePathIgnorePatterns: ['/node_modules/'],
+    testMatch: ['**/tests/**/*.test.js'],
+    setupFiles: ['<rootDir>/tests/setup.js']
+  };
